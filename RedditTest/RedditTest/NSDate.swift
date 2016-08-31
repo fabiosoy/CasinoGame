07e04c9f012e8b9433136bdaf07a2231dfc80673
Fabio Bermudez <Fabio@Fabios-iMac.local>
2016-08-31 18:09:10 +0200

    updating code

--- RedditTest/RedditTest/NSDate.swift
+++ RedditTest/RedditTest/NSDate.swift
@@ -12,37 +12,37 @@ import Foundation
 extension NSDate {
 
     func timeAgoSinceDate() -> String {
-        let calendar = NSCalendar.currentCalendar()
-        let now = NSDate()
-        let earliest = now.earlierDate(self)
+    
+                let now = NSDate()
+        let earliest = NSDate().earlierDate(self)
         let latest = (earliest == now) ? self : now
-        let components:NSDateComponents = calendar.components([NSCalendarUnit.Minute , NSCalendarUnit.Hour , NSCalendarUnit.Day , NSCalendarUnit.WeekOfYear , NSCalendarUnit.Month , NSCalendarUnit.Year , NSCalendarUnit.Second], fromDate: earliest, toDate: latest, options: NSCalendarOptions())
+        let components:NSDateComponents =  NSCalendar.currentCalendar().components([NSCalendarUnit.Minute , NSCalendarUnit.Hour , NSCalendarUnit.Day , NSCalendarUnit.WeekOfYear , NSCalendarUnit.Month , NSCalendarUnit.Year , NSCalendarUnit.Second], fromDate: earliest, toDate: latest, options: NSCalendarOptions())
         if (components.year >= 2) {
-            return "\(components.year) years ago"
+            return  String(format: "%d years ago", components.year)
         } else if (components.year >= 1){
            return "Last year"
         } else if (components.month >= 2) {
-            return "\(components.month) months ago"
+            return String(format: "%d months ago", components.month)
         } else if (components.month >= 1){
             return "Last month"
         } else if (components.weekOfYear >= 2) {
-            return "\(components.weekOfYear) weeks ago"
+            return String(format: "%d weeks ago", components.weekOfYear)
         } else if (components.weekOfYear >= 1){
             return "Last week"
         } else if (components.day >= 2) {
-            return "\(components.day) days ago"
+            return String(format: "%d days ago", components.day)
         } else if (components.day >= 1){
             return "Yesterday"
         } else if (components.hour >= 2) {
-            return "\(components.hour) hours ago"
+            return String(format: "%d hours ago", components.hour)
         } else if (components.hour >= 1){
             return "An hour ago"
         } else if (components.minute >= 2) {
-            return "\(components.minute) minutes ago"
+            return String(format: "%d minutes ago", components.minute)
         } else if (components.minute >= 1){
             return "A minute ago"
         } else if (components.second >= 3) {
-            return "\(components.second) seconds ago"
+            return String(format: "%d seconds ago", components.second)
         } else {
             return "Just now"
         }

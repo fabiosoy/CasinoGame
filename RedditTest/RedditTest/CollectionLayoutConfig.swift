e80db3baa9175a2c6999814b0216b995702cfdd1
Fabio Bermudez <Fabio@Fabios-iMac.local>
2016-08-31 02:36:17 +0200

    adding margin in collection view

--- RedditTest/RedditTest/CollectionLayoutConfig.swift
+++ RedditTest/RedditTest/CollectionLayoutConfig.swift
@@ -12,8 +12,10 @@ import UIKit
 // Collection Layout Configs
 struct CollectionLayoutConfig {
     static let NUMBER_CELL_ROWS : CGFloat = 2
-    static let CELL_WIDTH : CGFloat = CGFloat( CGFloat(ScreenSize.width) / CollectionLayoutConfig.NUMBER_CELL_ROWS)
+    static let CELL_MARGIN : CGFloat = 2
+    static let CELL_WIDTH : CGFloat = CGFloat( CGFloat(ScreenSize.width) / CollectionLayoutConfig.NUMBER_CELL_ROWS) - CollectionLayoutConfig.CELL_MARGIN
     static let CELL_MIN_HEIGHT : CGFloat = 205
     static let TEXT_MAX_HEIGHT : CGFloat = 5000
     static let FONT_SIZE : CGFloat = 15.0
+
 }
